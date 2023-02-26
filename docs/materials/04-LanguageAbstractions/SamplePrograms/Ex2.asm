954e11106916237e62ca22ad14bd6c2c14df251d
braughtg <braught@dickinson.edu>
2023-02-26 12:57:28 -0500

    Added LA sample programs for reference

---
+++ docs/materials/04-LanguageAbstractions/SamplePrograms/Ex2.asm
@@ -0,0 +1,19 @@
+* Assembly Language
+
+X:	.word 
+Z:	.word	
+	
+* This program reads X from STDIN
+* Computes Z = 2*X-1
+* Displays the result using STDOUT
+
+	LOAD R1 STDIN     * R1 <- MM[STDIN]
+	STORE R1 X        * MM[X] <- R1
+
+	ADD R2 R1 R1      * R2 <- 2* R1
+	SUB R2 R2 #1      * R2 <- R2 - 1
+	STORE R2 Z        * MM[Z] <- R2
+
+	STORE R2 STDOUT   * MM[STDOUT] <- R2
+	
+	HALT
---
+++ docs/materials/04-LanguageAbstractions/SamplePrograms/PrintRef.java
@@ -0,0 +1,10 @@
+public class PrintRef {
+	public static void main(String[] args) {
+
+		PrintRef pr = new PrintRef();
+		System.out.println(pr);
+
+		int x[] = {918, 321, 125};
+		System.out.println(x);
+	}
+}
